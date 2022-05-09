6036ca9fe2ec7540f73f7ca94fe92dfc60a8daf2
caleb-freitas <calebfreitas@tutanota.com>
2022-05-09 18:01:44 -0300

    refactor(main): add create statement usecase to compose simpler objects

--- src/main/factory/create.statement.controller.ts
+++ src/main/factory/create.statement.controller.ts
@@ -1,7 +1,17 @@
+import { DbCreateStatement } from "@data/usecases"
 import { CreateStatementRepository } from "@infra/database"
+import { LoadClientByIdRepository } from "@infra/database/load.client.by.id"
+import { UpdateClientAmountRepository } from "@infra/database/update.client.amount"
 import { CreateStatementController } from "@presentation/controllers"
 
 export const makeCreateStatementController = () => {
   const createStatementRepository = new CreateStatementRepository()
-  return new CreateStatementController(createStatementRepository)
+  const loadClientByIdRepository = new LoadClientByIdRepository()
+  const updateClientAmountRepository = new UpdateClientAmountRepository()
+  const dbCreateStatement = new DbCreateStatement(
+    loadClientByIdRepository,
+    createStatementRepository,
+    updateClientAmountRepository
+  )
+  return new CreateStatementController(dbCreateStatement)
 }
